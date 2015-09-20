8dce8984f8ba317bf9310082ce1bd88f31130995
Vincent Pruijn <VIP@Vincents-MacBook-Pro.local>
2015-09-21 01:00:09 +0200

    First

---
+++ mashup.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+echo "My name is: $0"
+echo "i received $# arguments"
+echo "i will make a mashup using $1"
+
+FILENAME=$1
+
+BASENAME = basename $1 .wav
+
+# intro
+sox $FILENAME ${BASENAME}_intro.wav trim 9.4 4.3
+
+# Reverse intro
+sox ${BASENSME}_intro.wav ${BASENAME}_rev.wav reverse
+
+# Reverb & reverse
+sox $FILENAME ${BASENAME}_revreb.wav reverse reverb
+
+# Mashup!!!!!!!!!!!!!
+sox --combine concatenate ${BASENAME}_intro.wav ${BASENAME}_rev.wav mashup.wav
+
+echo "Done"
